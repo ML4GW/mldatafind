88a722e7a45fa565e82fd757c204065743293380
ethan.marx <ethan.marx@ligo.org>
2022-11-01 05:05:14 -0700

    add kerberos

--- mldatafind/authenticate.py
+++ mldatafind/authenticate.py
@@ -4,18 +4,17 @@ from ciecplib.ui import get_cert
 from ciecplib.x509 import check_cert, load_cert, write_cert
 
 
-def authenticate(endpoint: str, username: str):
+def authenticate():
     """
     Authenticate a user to access LIGO data sources
 
     This function will load a X509 certificate from the environment
-    variable X509_USER_PROXY
+    variable X509_USER_PROXY. If not valid, it will generate a new credential.
+
     """
 
-    # load in certificate
     path = os.getenv("X509_USER_PROXY")
 
-    # if not set, raise value error
     if path is None:
         raise ValueError("Set X509_USER_PROXY environment variable")
     else:
@@ -23,11 +22,11 @@ def authenticate(endpoint: str, username: str):
         cert = load_cert(path)
 
         try:
+            raise RuntimeError
             # validate the certificate
             check_cert(cert)
         except RuntimeError:
-            # if certificate invalid get a new one and write it to path
-            cert, key = get_cert(endpoint=endpoint, username=username)
+            cert, key = get_cert(kerberos=True)
 
             write_cert(path, cert, key)
 
